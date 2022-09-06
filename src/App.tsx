4ed4eb748aac3ee8d7076e17205249e6a766e31e
c4nnh <cannh.99@gmail.com>
2022-09-06 17:41:29 +0700

    add redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { PrivatePages } from "./pages";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { END_POINTS } from "./constants/endpoints";
+import { AuthPages, PrivatePages } from "./pages";
 
 export const App: React.FC = () => {
   // if (1) return <Navigate to={END_POINTS.AUTH.MASTER} replace />;
 
-  return <PrivatePages />;
+  return (
+    <Routes>
+      <Route path={`${END_POINTS.AUTH.MASTER}/*`} element={<AuthPages />} />
+      <Route
+        path={`${END_POINTS.PRIVATE.MASTER}/*`}
+        element={<PrivatePages />}
+      />
+      <Route
+        path="/*"
+        element={<Navigate to={END_POINTS.PRIVATE.MASTER} replace />}
+      />
+    </Routes>
+  );
 };
